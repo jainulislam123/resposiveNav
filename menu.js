856a28cb5eb8c20de472f75ae86648f6236c70be
jainulislam123 <islamjainul9@gmail.com>
2024-03-23 21:25:16 +0530

    first commit

---
+++ menu.js
@@ -0,0 +1,19 @@
+let icon = document.querySelector(".icons");
+let navBar = document.querySelector(".navBar");
+let icon1 = document.querySelector(".fa-bars");
+let icon2 = document.querySelector(".fa-xmark");
+let x=0;
+icon.addEventListener("click",()=>{
+ if(x==0){
+    x=1;
+    // navBar.classList.add("active");
+    icon1.style.display="none"
+    icon2.style.display="block"
+ }else{
+    // navBar.classList.remove("active");
+    icon1.style.display="block"
+    icon2.style.display="none"
+    x=0;
+ }
+ navBar.classList.toogle("active"); // IF single click
+})
